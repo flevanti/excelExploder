a6c39dd9d5fe8de20853ecaa2143644b3660278e
flevanti <frenke@frenke.it>
2018-12-27 13:17:46 +0000

    playing

--- main.go
+++ main.go
@@ -2,19 +2,33 @@ package main
 
 import (
 	"fmt"
-	"github.com/flevanti/isAwsLambda"
+	"github.com/flevanti/mongodbClient"
 	_ "github.com/joho/godotenv/autoload"
-	"mongoClient"
 )
 
 func main() {
-	connected, err := mongodbClient.Connect("mongodb:fdfdsfdfs", "fdsfs");
-	if err != nil || connected != true {
+	mdb, err := mongodbClient.Connect("mongodb+srv://appuser:appuser@cluster0-nnhfc.mongodb.net/test?retryWrites=true", "testdb");
+	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	defer mdb.Close()
+	collections, err := mdb.RetrieveCollectionsList()
+	if (err != nil) {
+		fmt.Println(err.Error())
+		return
+	}
+	if len(collections) > 0 {
+		fmt.Println("Collections found:")
+		for _, collection := range collections {
+			fmt.Println("- " + collection)
+		}
+	} else {
+		fmt.Println("No collections found")
+	}
 
-	fmt.Printf("hello %v", isAwsLambda.IsItInitialised())
-	fmt.Printf("hello %v", isAwsLambda.IsItLambda())
-	fmt.Printf("hello %v", isAwsLambda.IsItInitialised())
+	//fmt.Printf("hello %v", isAwsLambda.IsItInitialised())
+	//fmt.Printf("hello %v", isAwsLambda.IsItLambda())
+	//fmt.Printf("hello %v", isAwsLambda.IsItInitialised())
+
 }
