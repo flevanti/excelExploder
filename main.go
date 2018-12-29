7fcef7cd62c7634dede84762f3e54b1d7fe518f4
flevanti <frenke@frenke.it>
2018-12-29 10:09:36 +0000

    playing

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -10,14 +9,13 @@ import (
 	"github.com/flevanti/isAwsLambda"
 	"github.com/flevanti/mongodbClient"
 	_ "github.com/joho/godotenv/autoload"
-	"github.com/mongodb/mongo-go-driver/bson"
 	"os"
 	"time"
 )
 
 // this is the typical payload used by S3 to trigger aws lambda
 // LocalPayload* elements are added to use the same structure if we want to test it outside of AWS
-type payloadType struct {
+type PayloadType struct {
 	LocalPayload     bool   `json:"localPayload"`
 	LocalPayloadFile string `json:"localPayloadFile"`
 	Records          []struct {
@@ -56,7 +54,11 @@ type payloadType struct {
 	} `json:"Records"`
 }
 
+var dbConn mongodbClient.ConnType
+var err error
+
 func main() {
+
 	if isAwsLambda.IsItLambda() {
 		// AWS lambda will add the payload to the handler call, we just need to specify the handler fn name...
 		// If the lambda is the local docker implementation, we need to pass the payload (a dummy one) as an argument
@@ -68,80 +70,92 @@ func main() {
 			fmt.Printf("Unable to load dummy payload: %s 💥 💥 💥 \n", err)
 			return
 		}
-		Handler(payload)
-	}
-}
-
-func Handler(payload payloadType) {
-
-	mdb, err := mongodbClient.Connect(os.Getenv("DBCONNSTRING"), os.Getenv("DBDATABASE"))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer mdb.Close()
-	collections, err := mdb.RetrieveCollectionsList()
-	if (err != nil) {
-		fmt.Println(err.Error())
-		return
-	}
-	if len(collections) > 0 {
-		fmt.Println("Collections found:")
-		for _, collection := range collections {
-			fmt.Println("- " + collection)
+		output, err := Handler(payload)
+		println(output)
+		if err != nil {
+			println("============================")
+			println("ERROR WHILE PROCESSING REQUEST:")
+			println(err.Error())
+			println("============================")
 		}
-	} else {
-		fmt.Println("No collections found")
 	}
-
-	result, err := mdb.Db.Collection("hellothere").InsertMany(
-		context.Background(),
-		[]interface{}{bson.D{
-			{"item", "canvas"},
-			{"qty", 100},
-			{"tags", bson.A{"cotton", "jeans"}},
-			{"size", bson.D{
-				{"h", 28},
-				{"w", 35.5},
-				{"uom", "cm"},
-			}},
-		},
-			bson.D{
-				{"item", "another canvas"},
-				{"qty", 100},
-				{"tags", bson.A{"plastic", "mercury"}},
-				{"size", bson.D{
-					{"h", 28},
-					{"w", 35.5},
-					{"uom", "cm"},
-				}},
-			},
-		})
-	for _, i := range result.InsertedIDs {
-		fmt.Println(i)
-	}
-	//fmt.Printf("hello %v", isAwsLambda.IsItInitialised())
-	//fmt.Printf("hello %v", isAwsLambda.IsItLambda())
-	//fmt.Printf("hello %v", isAwsLambda.IsItInitialised())
-
 }
 
-func LoadDummyPayload() (payloadType, error) {
+func Handler(payload PayloadType) (string, error) {
+	err = connectToDb()
+	if err != nil {
+		return "", err
+	}
+
+	return "OK!", nil
+}
+
+func LoadDummyPayload() (PayloadType, error) {
 	dummyPayloadFile := os.Getenv("DUMMYPAYLOADFILE")
 	if len(dummyPayloadFile) == 0 {
-		return payloadType{}, errors.New("dummy payload filename not found in configuration file")
+		return PayloadType{}, errors.New("dummy payload filename not found in configuration file")
 	}
 	if goUtils.FileExists(dummyPayloadFile) == false {
-		return payloadType{}, errors.New(fmt.Sprintf("dummy payload filename provided `%s` not found", dummyPayloadFile))
+		return PayloadType{}, errors.New(fmt.Sprintf("dummy payload filename provided `%s` not found", dummyPayloadFile))
 	}
 	dummypayloadfileContent, err := goUtils.ReadFileContent(dummyPayloadFile)
 	if err != nil {
-		return payloadType{}, err
+		return PayloadType{}, err
 	}
-	var payload payloadType
+	var payload PayloadType
 	err = json.Unmarshal([]byte(dummypayloadfileContent), &payload)
 	if err != nil {
-		return payloadType{}, err
+		return PayloadType{}, err
 	}
 	return payload, nil
+}
+
+func connectToDb() error {
+	dbConn, err := mongodbClient.Connect(os.Getenv("DBCONNSTRING"), os.Getenv("DBDATABASE"))
+	if err != nil {
+		return err
+	}
+	defer dbConn.Close()
+
+	//collections, err := dbConn.RetrieveCollectionsList()
+	//if err != nil {
+	//	return err
+	//}
+	//if len(collections) > 0 {
+	//	fmt.Println("Collections found:")
+	//	for _, collection := range collections {
+	//		fmt.Println("- " + collection)
+	//	}
+	//} else {
+	//	fmt.Println("No collections found")
+	//}
+
+	//result, err := dbConn.Db.Collection("hellothere").InsertMany(
+	//	context.Background(),
+	//	[]interface{}{bson.D{
+	//		{"item", "canvas"},
+	//		{"qty", 100},
+	//		{"tags", bson.A{"cotton", "jeans"}},
+	//		{"size", bson.D{
+	//			{"h", 28},
+	//			{"w", 35.5},
+	//			{"uom", "cm"},
+	//		}},
+	//	},
+	//		bson.D{
+	//			{"item", "another canvas"},
+	//			{"qty", 100},
+	//			{"tags", bson.A{"plastic", "mercury"}},
+	//			{"size", bson.D{
+	//				{"h", 28},
+	//				{"w", 35.5},
+	//				{"uom", "cm"},
+	//			}},
+	//		},
+	//	})
+	//for _, i := range result.InsertedIDs {
+	//	fmt.Println(i)
+	//}
+
+	return nil
 }
